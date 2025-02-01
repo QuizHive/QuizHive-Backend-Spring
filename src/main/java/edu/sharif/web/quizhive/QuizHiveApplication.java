bf8d701c79f5dc5bb28401a27a2e941d4defee56
AHMZ <ahmz.ut@gmail.com>
2025-02-01 16:58:09 +0330

    Add Cors

--- src/main/java/edu/sharif/web/quizhive/QuizHiveApplication.java
+++ src/main/java/edu/sharif/web/quizhive/QuizHiveApplication.java
@@ -2,10 +2,24 @@ package edu.sharif.web.quizhive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class QuizHiveApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuizHiveApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// Allow all origins, headers, and methods
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+			}
+		};
 	}
 }
